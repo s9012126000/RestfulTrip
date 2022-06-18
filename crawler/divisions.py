97e3e18a33db9060c50efba69a972da0e587e6ba
Jeff Chen <s9012126000@gmail.com>
2022-06-18 11:58:26 +0800

    add crawler for taiwan divisions

---
+++ crawler/divisions.py
@@ -0,0 +1,18 @@
+from config.crawler_config import *
+import json
+
+
+def get_divisions():
+    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
+    driver.delete_all_cookies()
+    URL_LOC = 'https://zh.wikipedia.org/wiki/中華民國臺灣地區鄉鎮市區列表'
+    driver.get(URL_LOC)
+    div = driver.find_elements(By.XPATH, "//div[@id='mw-content-text']/div[1]/table[7]/tbody/tr/td[1]/small")
+    divs = set([x.text for x in div])
+    driver.quit()
+    return divs
+
+
+divisions = get_divisions()
+with open('jsons/divisions.json', 'w') as f:
+    json.dump(list(divisions), f)
