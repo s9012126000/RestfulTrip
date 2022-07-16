a5df36925d1a1ba9f110698cd952253fe72a3ef5
Jeff Chen <s9012126000@gmail.com>
2022-07-16 23:06:46 +0800

    [crawler][hotels] change strategy of opening url

--- crawler/hotels.py
+++ crawler/hotels.py
@@ -22,18 +22,16 @@ class Worker(threading.Thread):
         URL = f'https://tw.hotels.com/Hotel-Search?destination={div}&startDate=2022-10-01&endDate=2022-10-02&rooms=1&adults=1'
         self.driver.get(URL)
         print(f"{self.worker_num}", URL)
-        action = ActionChains(self.driver)
 
         def get_cards():
             last_len = 0
             while True:
-
-                self.driver.execute_script("window.scrollTo(0, 500000)")
+                time.sleep(1)
+                self.driver.execute_script("window.scrollTo(0, 50000)")
                 button = WebDriverWait(self.driver, 5).until(
                     ec.element_to_be_clickable((By.XPATH, "//button[@data-stid='show-more-results']"))
                 )
-                action.move_to_element(button).perform()
-                time.sleep(2)
+                time.sleep(3)
                 button.click()
                 time.sleep(1)
                 check = len(self.driver.find_elements(By.XPATH, "//section[@class='results']/ol/li"))
@@ -46,10 +44,24 @@ class Worker(threading.Thread):
             return card
         try:
             cards = get_cards()
-        except:
+        except TimeoutException:
             cards = self.driver.find_elements(
                 By.XPATH, "//section[@class='results']/ol/li/div/a[@data-stid='open-hotel-information']")
-            print(f'division {div} fail')
+            for i in range(5):
+                try:
+                    print(self.worker_num, 'attempt', i + 1)
+                    self.driver.refresh()
+                    cards = get_cards()
+                    break
+                except TimeoutException:
+                    print(self.worker_num, 'attempt', i + 1, 'fail')
+                    if i == 4:
+                        cards = self.driver.find_elements(
+                            By.XPATH, "//section[@class='results']/ol/li/div/a[@data-stid='open-hotel-information']")
+                        print(f'division {div} fail')
+                        print(f'{self.worker_num} scan_done')
+                        with open('logs/hotels/hotels_lost_data.log', 'a') as e:
+                            e.write('url:\n' + str(URL) + '\n')
         cards = [x.get_attribute('href') for x in cards]
         print(f"worker {self.worker_num}: <<{div}>> {len(cards)} cards \n-------------------------------")
         hotel_ls = []
@@ -61,19 +73,14 @@ class Worker(threading.Thread):
         for c in range(len(cards)):
             try:
                 pack = self.fetch(cards[c], c)
-            except:
+            except TimeoutException:
                 continue
             hotel_ls.append(pack)
         col.insert_many(hotel_ls, bypass_document_validation=True)
 
     def fetch(self, url, n):
-        self.driver.execute_script("window.open()")
-        WebDriverWait(self.driver, 10).until(ec.number_of_windows_to_be(2))
-        self.driver.switch_to.window(self.driver.window_handles[1])
         self.driver.get(url)
-        time.sleep(1)
-        self.driver.switch_to.window(self.driver.window_handles[1])
-        detail_elm = WebDriverWait(self.driver, 60).until(
+        detail_elm = WebDriverWait(self.driver, 15).until(
             ec.presence_of_element_located((By.XPATH, "//div[@data-stid='content-hotel-title']"))
         )
         detail = detail_elm.text
@@ -100,8 +107,6 @@ class Worker(threading.Thread):
         }
         name = detail.split('\n')[0]
         print(f"worker {self.worker_num}: {n} {name}")
-        self.driver.close()
-        self.driver.switch_to.window(self.driver.window_handles[0])
         return pack
 
 
