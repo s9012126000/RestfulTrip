92dfc6818008208525d7b13b24b8527ba1916e5b
Jeff Chen <s9012126000@gmail.com>
2022-07-31 02:57:45 +0800

    [server] fix checkin date limit over 14 days

--- app/server/utils/utils.py
+++ app/server/utils/utils.py
@@ -4,7 +4,7 @@ import datetime
 def get_date_limitation(day):
     checkin = datetime.datetime.now().date().isoformat()
     checkin_limit = (
-        (datetime.datetime.now() + datetime.timedelta(days=day)).date().isoformat()
+        (datetime.datetime.now() + datetime.timedelta(days=day-1)).date().isoformat()
     )
     checkout = (datetime.datetime.now() + datetime.timedelta(days=1)).date().isoformat()
     checkout_limit = (
